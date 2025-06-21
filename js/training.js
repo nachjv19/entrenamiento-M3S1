23f7c82903d76de679f19076aef0c6ffb6e28512
nachjv19 <nachjvasquez@gmail.com>
2025-06-21 16:12:02 -0500

    training.js

--- js/training.js
+++ js/training.js
@@ -1,48 +1,56 @@
-function IniProg() {
-  let name = "";
-  let age = 0;
-  let ValidName = false;
-  let ValidAge = false;
+function iniApp() {
+    let name = " ";
+    let age = 0;
+    let checkName = false;
+    let checkAge = false;
 
-  // Name input and validation loop
-  while (!ValidName) {
-    name = prompt("Please enter your name:");
+    while(!checkName){
+      name = prompt("What is your Name?: ")
 
-    if (name === null) {
-      alert("❎ Operation cancelled. Goodbye!");
-      return;
+        if (name === null) {
+            alert("closed app");
+            return;
+        }
+
+     if(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(name.trim())) {
+        name = name.trim();
+         ValidName = true;
+         } else {
+         alert("Error: letters only.")
+        }
     }
 
-    if (/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(name.trim())) {
-      name = name.trim();
-      ValidName = true;
-    } else {
-      alert("❌ Invalid name. Please use letters only and do not leave it blank.");
+    while(!checkAge){
+        const ageInput = prompt("What is your age?: ")
+        if(ageInput === null){
+            alert("closed app");
+            return;
+        }
+        age = Number(ageInput);
+
+
+        if (!isNaN(age)&& age > 0){
+            ValidAge = true
+        }else {
+            alert("Error: numbers only");
+        }
     }
-  }
 
-  // Age input and validation loop
-  while (!ValidAge) {
-    const ageInput = prompt("Please enter your age:");
 
-    if (ageInput === null) {
-      alert("❎ Operation cancelled. Goodbye!");
-      return;
+    if(age>=18){
+        alert(`${name}, adult`);
+    }else {
+        alert (`${name}, child`);
     }
-
-    age = Number(ageInput);
-
-    if (!isNaN(age) && age > 0) {
-      ValidAge = true;
-    } else {
-      alert("❌ Invalid age. Please enter a number greater than 0.");
-    }
-  }
-
-  // Final result
-  if (age >= 18) {
-    alert(`${name}, you are an adult.`);
-  } else {
-    alert(`${name}, you are a minor.`);
-  }
 }
+
+
+
+
+    
+
+
+
+
+
+
